dba306d671b858104d9b67328a49a6fed4a20694
fwcd <fwcdmail@gmail.com>
2018-11-26 08:09:46 +0100

    Added negation

---
+++ src/main/java/com/fwcd/circuitbuilder/model/logic/Negation.java
@@ -0,0 +1,22 @@
+package com.fwcd.circuitbuilder.model.logic;
+
+/**
+ * A logical NOT.
+ */
+public class Negation implements LogicExpression {
+	private final LogicExpression value;
+	
+	public Negation(LogicExpression value) {
+		this.value = value;
+	}
+	
+	@Override
+	public int getInputCount() {
+		return 2;
+	}
+	
+	@Override
+	public boolean evaluate() {
+		return !value.evaluate();
+	}
+}
