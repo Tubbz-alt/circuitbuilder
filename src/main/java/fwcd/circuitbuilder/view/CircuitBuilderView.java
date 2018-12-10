345854a76a912db724b30c4cac7a2dbd93e6f897
fwcd <fwcdmail@gmail.com>
2018-12-10 17:13:33 +0100

    Added formula editor pane

--- src/main/java/fwcd/circuitbuilder/view/CircuitBuilderView.java
+++ src/main/java/fwcd/circuitbuilder/view/CircuitBuilderView.java
@@ -4,9 +4,11 @@ import java.awt.BorderLayout;
 
 import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 
 import fwcd.circuitbuilder.model.CircuitBuilderModel;
 import fwcd.circuitbuilder.utils.BackgroundLooper;
+import fwcd.circuitbuilder.view.formula.FormulaEditorView;
 import fwcd.fructose.swing.View;
 
 /**
@@ -17,8 +19,9 @@ public class CircuitBuilderView implements View {
 	private final JPanel component;
 	private final int tickDelay = 80; // ms tick delay
 	
-	private CircuitToolsPanel itemPanel;
-	private CircuitGridView grid;
+	private final CircuitToolsPanel itemPanel;
+	private final CircuitGridView grid;
+	private final FormulaEditorView formulaEditor;
 	
 	// TODO: Serialization
 	
@@ -26,11 +29,21 @@ public class CircuitBuilderView implements View {
 		component = new JPanel();
 		component.setLayout(new BorderLayout());
 		
+		JSplitPane contentWithEditor = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+		
+		JSplitPane content = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+		contentWithEditor.setTopComponent(content);
+		
 		itemPanel = new CircuitToolsPanel(model, context);
 		component.add(itemPanel.getComponent(), BorderLayout.WEST);
 		
 		grid = new CircuitGridView(model.getGrid(), context);
-		component.add(grid.getComponent(), BorderLayout.CENTER);
+		content.setLeftComponent(grid.getComponent());
+		
+		formulaEditor = new FormulaEditorView();
+		contentWithEditor.setBottomComponent(formulaEditor.getComponent());
+		
+		component.add(contentWithEditor, BorderLayout.CENTER);
 		
 		new BackgroundLooper("Circuit engine", tickDelay, model.getEngine()::tick).start();
 	}
---
+++ src/main/java/fwcd/circuitbuilder/view/formula/FormulaEditorView.java
@@ -0,0 +1,17 @@
+package fwcd.circuitbuilder.view.formula;
+
+import javax.swing.JComponent;
+import javax.swing.JTextArea;
+
+import com.fwcd.fructose.swing.View;
+
+public class FormulaEditorView implements View {
+	private final JTextArea component;
+	
+	public FormulaEditorView() {
+		component = new JTextArea();
+	}
+	
+	@Override
+	public JComponent getComponent() { return component; }
+}
