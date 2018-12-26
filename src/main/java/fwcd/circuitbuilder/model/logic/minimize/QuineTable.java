b2451025b53592bcd57c64df083d3d0de1f0d02a
fwcd <fwcdmail@gmail.com>
2018-12-26 21:56:29 +0100

    Added QuineTable class

--- src/main/java/fwcd/circuitbuilder/model/logic/minimize/QuineTable.java
+++ src/main/java/fwcd/circuitbuilder/model/logic/minimize/QuineTable.java
@@ -1,9 +1,18 @@
 package fwcd.circuitbuilder.model.logic.minimize;
 
+import java.util.Set;
+
+import fwcd.fructose.structs.MapTable;
+import fwcd.fructose.structs.Table;
+
 /**
  * A prime implication chart that may be used
  * to elimite redundant prime implicants.
  */
 public class QuineTable {
+	private final Table<Set<Integer>, Set<Integer>, Boolean> data = new MapTable<>();
 	
+	public QuineTable() {
+		
+	}
 }
